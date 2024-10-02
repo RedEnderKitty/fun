d55e245ec79d1dd3ba8275d659b200c371e26bbd
RedEnderKitty <redenderkitty@gmail.com>
2024-10-02 23:36:57 +0000

    Javascript

---
+++ script.js
@@ -0,0 +1,22 @@
+// Define HTML elements
+const board = document.getElementById('game-board');
+
+// Define game bariables
+let snake = [{ x: 10, y: 10 }]
+//Draw Game App
+function draw() {
+    board.innerHTML = '';
+    drawSnake();
+}
+
+//Draw Snake
+function drawSnake() {
+    snake.forEach((segment) => {
+        const snakeElement = createGameElement('div', 'snake');
+    });
+}
+
+// Create a snake or food cube/div
+function createGameElement(tag, className) {
+    const element = document.createElement(tag);
+}
